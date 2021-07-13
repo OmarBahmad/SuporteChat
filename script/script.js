f1ea5391c44f1ff52ec043acc60bb724a43c3a13
OmarBahmad <omarbahmad@gmail.com>
2021-07-13 16:33:57 -0300

    .

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ function ler(n){
     let campo = document.getElementsByClassName('campo')
 
     let solucao = document.getElementsByClassName('solucao')
-     for(i = 0; i < solucao.length; i++ ){
+     for(i = 0; i <= solucao.length; i++ ){
          solucao[i].className= 'solucao hidden'
      }
     solucao[n].className = 'solucao'
