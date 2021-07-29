f9e62386b9a575b46be161f8f316530b84a2869a
OmarBahmad <omarbahmad@gmail.com>
2021-07-29 15:06:31 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -6,12 +6,12 @@
 //     solucao[n].style.display = 'show';
 // }
 
-function ler(n){
-    let campo = document.getElementsByClassName('campo')
+function ler(n) {
+  let campo = document.getElementsByClassName("campo");
 
-    let solucao = document.getElementsByClassName('solucao')
-     for(i = 0; i < solucao.length; i++ ){
-         solucao[i].className= 'solucao hidden'
-     }
-    solucao[n].className = 'solucao'
+  let solucao = document.getElementsByClassName("solucao");
+  for (i = 0; i < solucao.length; i++) {
+    solucao[i].className = "solucao hidden";
+  }
+  solucao[n].className = "solucao";
 }
